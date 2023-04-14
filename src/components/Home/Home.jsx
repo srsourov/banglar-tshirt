47f272200f5c970747a4ed5787ae255755402b92
SR Sourov <sourov.web@gmail.com>
2023-04-14 15:51:01 +0600

    react router set and routes set

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import { Outlet, useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const tshirts = useLoaderData();
+    
+    return (
+        <div>
+            <h2>This is home {tshirts.length}</h2>
+        </div>
+    );
+};
+
+export default Home;
